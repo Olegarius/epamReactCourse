ef15042cfb8c87f5074811e1011e29f3727e3ab4
oleh.murenkyi <oleh.murenkyi@pl.abb.com>
2022-05-03 12:56:02 +0300

    task 1 completed

---
+++ src/components/ComponentExample/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class ComponentExample extends React.Component {
+    render() {
+      return <h1>Hello, {this.props.name}!</h1>;
+    }
+  }
+  export default ComponentExample;
